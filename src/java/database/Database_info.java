76942be2fcc1d99297faac7cb1d0cad1e06892fd
shafaetjsr <shafaet811@gmail.com>
2016-11-07 19:39:15 +0600

    Update Database_info.java

--- src/java/database/Database_info.java
+++ src/java/database/Database_info.java
@@ -4,5 +4,5 @@ public interface Database_info {
  String DRIVER="com.mysql.jdbc.Driver";
  String CON_STRING="jdbc:mysql://localhost:3306/office_attend";
  String USER="root";
- String PASSWORD="811044";
+ String PASSWORD="xxxx";
 }
